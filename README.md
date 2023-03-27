010fa1bbcc900c92a4e96e4127298d1fdde253d1
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-03-26 23:03:48 -0500

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# proyectoBiometricSer
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      LOGIN
+      <br/>
+      <Link href="/homeAprendices">Ingresar</Link>
+    </div>
+  )
+}
